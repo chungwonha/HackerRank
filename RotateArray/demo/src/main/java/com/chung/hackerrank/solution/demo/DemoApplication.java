0ec20d0e089135f387d16ae02be4580257b923a2
chungha@yahoo.com <chungha@yahoo.com>
2021-02-16 23:11:44 -0500

    solved

--- RotateArray/demo/src/main/java/com/chung/hackerrank/solution/demo/DemoApplication.java
+++ RotateArray/demo/src/main/java/com/chung/hackerrank/solution/demo/DemoApplication.java
@@ -3,8 +3,10 @@ package com.chung.hackerrank.solution.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -23,9 +25,86 @@ public class DemoApplication {
 		}
 		a = linkedList.stream().mapToInt(i->i).toArray();
 
+		int[] aa = {1,2,4,2,44,55};
+		int[] sorted_p = Arrays.stream(aa).sorted().toArray();
+
+		System.out.println("-> "+Math.round(6/2));
+
 		return a;
 
 	}
+
+	static int activityNotifications(int[] expenditure, int d) {
+
+		int notificationCounter=0;
+
+		for(int i=0;i<expenditure.length;i++){
+			int[] subArr = new int[d];
+			if(i+d==expenditure.length){
+				break;
+			}
+
+			for(int j=0;j<d;j++){
+				subArr[j]=expenditure[i+j];
+			}
+
+			int[] sorted_e = Arrays.stream(subArr).sorted().toArray();
+			double median;
+			if(d%2==0){
+				//even number
+				int num = d/2;
+				median = (sorted_e[num]+sorted_e[num-1])/2.0;
+			}else{
+				//odd number
+				median = sorted_e[Math.round(d/2)];
+			}
+
+			if((2*median)<=expenditure[d+i]){
+				notificationCounter++;
+			}
+		}
+		return notificationCounter;
+
+	}
+
+	static int sherlockAndAnagrams(String s) {
+
+		          /*
+          abba
+          abcd
+          1. substring and grap more letter for each cycle
+                ->substring(0,1)
+                    substring(1,2)
+                    substring(2,3)
+                    substring(3,4)
+                ->substring(0,2)
+                    substring(1,3)
+                    substring(2,4)
+                ->substring(0,3)
+                  substring(1,4)
+
+          2. calculate sum of ascii value
+          3. put in hashmap with the summed ascii value as a key
+             (12:{"ab","ba"})
+
+          4. find
+          */
+		int strLengthIncr=0;
+		HashMap<String, String> stringBucket = new HashMap<>();
+
+		for(int i=1;i<=s.length();i++){
+			for(int j=0;j<s.length();j++) {
+				if(j+i>s.length()){
+					break;
+				}
+				String subStr = s.substring(j, j+i);
+
+			}
+		}
+		Arrays.stream(stringBucket.keySet().toArray()).forEach(a->System.out.println(a+"->"+stringBucket.get(a)));
+		return 0;
+	}
+
 
 //	static int[] rotateLeft2(int[] a, int d){
 //		int[] aa = new int[d];
--- RotateArray/demo/src/test/java/com/chung/hackerrank/solution/demo/DemoApplicationTests.java
+++ RotateArray/demo/src/test/java/com/chung/hackerrank/solution/demo/DemoApplicationTests.java
@@ -2,8 +2,11 @@ package com.chung.hackerrank.solution.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sun.java2d.pipe.SolidTextRenderer;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -20,5 +23,90 @@ class DemoApplicationTests {
 
 //		int[] a2 = {1, 2, 3, 4, 5};
 //		DemoApplication.rotateLeft2(a2,4);
+	}
+
+	@Test
+	void testActivityNotifications(){
+//		int[] a = {1, 2, 3, 4, 4};
+//		int i = DemoApplication.activityNotifications(a,4);
+//		System.out.println("i: "+i);
+//		int[] b = {2, 3, 4, 2, 3, 6, 8, 4, 5};
+//		int[] c = new int[b.length];
+//
+// 		for(int k=0;k<b.length;k++) {
+//				c[b[k]]++;
+//			}
+//
+//		Arrays.stream(c).forEach(n->System.out.print(n+" "));
+//		System.out.println("");
+//
+//		for(int j=1;j<c.length;j++){
+//			c[j] += c[j-1];
+//		}
+//		Arrays.stream(c).forEach(n->System.out.print(n+" "));
+//		System.out.println("");
+//
+//		for(int j=c.length-2;j>=0;j--){
+//			 c[j+1] =c[j];
+//		}
+//		Arrays.stream(c).forEach(n->System.out.print(n+" "));
+//		System.out.println("");
+//
+//		int[] s = new int[b.length];
+//		for(int j=0;j<b.length;j++){
+//			int index_of_n = c[b[j]];
+//			c[b[j]]++;
+//			s[index_of_n]=b[j];
+//		}
+//		Arrays.stream(s).forEach(n->System.out.print(n+" "));
+//		System.out.println("");
+//
+//		int studentsOnTime = Arrays.stream(a).filter(q->q>0).reduce(Integer::sum).getAsInt();
+//
+//
+//		String[] magazine = {"give", "me", "one", "grand", "today", "night"};
+//		String[] note = {"give", "one", "grand", "today"};
+//		HashMap<String,Integer> maps = new HashMap();
+//		for(String ss:magazine) {
+//			maps.put(ss,maps.get(ss)==null?1:maps.get(ss)+1);
+//		}
+//
+//		Arrays.stream(maps.keySet().stream().toArray()).forEach(sss->{System.out.println(maps.get(sss));});
+//		String YesNo="Yes";
+//		for(String n:note){
+//			if(maps.get(n)==null || maps.get(n)==0){
+//				YesNo="No";
+//				break;
+//			}else{
+//				Integer newCount =maps.get(n)-1;
+//
+//			}
+//		}
+//
+//		System.out.println(YesNo);
+
+		String sssss = "sqwert";
+		String[] s_arr = sssss.split("");
+		Arrays.stream(s_arr).forEach(System.out::println);
+		String s12 = "abab";
+		char[] charArr = s12.toCharArray();
+
+		for(int q=0;q<s12.length();q++){
+			String sbuStr1 = s12.substring(0,q+1);
+
+			for(int w=q+1;(w+sbuStr1.length())<=s12.length();w++){
+				String subStr2 = s12.substring(w,w+sbuStr1.length());
+				System.out.println(subStr2);
+			}
+
+		}
+
+
+	}
+
+	@Test
+	void testsherlockAndAnagrams(){
+		DemoApplication.sherlockAndAnagrams("abba");
+
 	}
 }
